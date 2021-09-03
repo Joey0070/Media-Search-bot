3057b0c3a11d543a4e1bab8fd9ea1e6b3b62df75
Joey0070 <88881246+Joey0070@users.noreply.github.com>
2021-09-03 19:52:04 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -16,7 +16,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='♦️ READ THIS INSTRUCTION ♦️\n✪ ഫയലുകൾ ലഭിക്കുന്നതിനായി  നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ join ചെയ്യണം😊\n✪ You Need To Join Our Channel and Press Refresh Button to get the File.',
                            switch_pm_parameter="subscribe")
         return
 
